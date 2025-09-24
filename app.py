2b22cab7e52c043398f1416267190fe469e98982
CodeKami0 <zademeadows2030@gmail.com>
2025-09-24 16:19:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -198,4 +198,4 @@ def protected():
     return jsonify({'message': 'This is a protected route', 'user': current_user}), 200
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
